f9719efc7c046863450c96ce981ce288d5daaef8
Mayank <mayankindoliya@gmail.com>
2024-04-12 12:15:07 +0530

    99. Handling Events & Using two way Bindings.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 const App = () => {
   return (
-    <Header />
-  )
-}
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
+};
 
-export default App
+export default App;
